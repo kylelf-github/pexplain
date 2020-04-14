ebe4abc973662d7041a7ef4adc4fa7b25f2ec0d4
Kyle Hailey <kylelf@gmail.com>
2020-04-14 12:30:58 -0700

    second commit

--- parse.py
+++ parse.py
@@ -1,4 +1,5 @@
 
+from array import array
 
 def main (): 
 	# read file
@@ -8,42 +9,59 @@ def main ():
 
 	# look for patterns
 	buffer=""
+	buffers=0
 	printbuf=0;
+        costdict={}
+        nodes={}
 	for line in lines:
 		# look for nodes
-		depth = line.find ("  ->")/6 
 		# depth equals -1, even though divided by 6
-		# print("depth ", depth)
+		depth = line.find ("  ->")/6 
+		
+		#  extract cost from line
 		costs = line.find ("cost")
 		if costs != -1 & depth == -1 :
 			total_cost=line.split("..")[1].split()[0]
 			print("first cost ", total_cost)
 			ccost=total_cost
+
 		# if "->" 
 		if depth != -1:
+			buffers=buffers+1
 			# split after first (
 			operation=line.split("(")[0]
 
-			pcost=ccost
+			prevcost=ccost
 
 			# split after first .., take right, split on blanks, take first string
 			ccost=line.split("..")[1].split()[0]
+			costdict[buffers]=ccost
+			print("costdict[",buffers,"]")
+
+			if buffers > 1:
+				print("costdelta=",costdict[buffers-1],"-",ccost)
+				costdelta=float(costdict[buffers-1])-float(ccost)
+				print("costdelta ", round(costdelta,2));
 
 			# percentage cost
 			pctcost=round(100*float(ccost)/float(total_cost),2)
 			if pctcost > 10.0 :
 				print(depth,ccost, round(100*float(ccost)/float(total_cost),2))
-				#print(depth,operation,ccost, round(100*float(ccost)/float(total_cost),2))
-				#print(line,)
-				#print(buffer)
-				#printbuf=1;
-				#if printbuf == 1:
-				#print(" -----vvvvvv--------- " )
 				print(buffer)
-				#printbuf=0;
+				nodes[buffers]=buffer
+				maxbuffer=buffers
+				print("nodes ",maxbuffer)
 				buffer=""
-				#print(" -----^^^^^^--------- " )
 		buffer=buffer + line
-	# display results
+
+        start=1
+	while start <= maxbuffer:
+		if start < maxbuffer:
+			nodecost=float(costdict[start])-float(costdict[start+1])
+			print("nodecost ",round(nodecost,2))
+		else:
+			print("nodecost ",costdict[start])
+		print nodes[start]
+		start += 1;
 
 main()
